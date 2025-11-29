3c3cb5e0701b04d5c20fab9d31821c5412505b0f
Gutbub2028 <101802448+Gutbub2028@users.noreply.github.com>
2025-11-29 11:49:20 +0000

    Initial commit

---
+++ sultan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main (){
+int age = 50;
+bool student = true;
+char grade = 'C';
+double gpa = 1.5;
+double sum= (gpa+2.5+3.5+0.5)/4;
+std::string name="Abood";
+std::cout<< "اسمي هو " << name << "عمري هو " << age << "عاما" << "انا طالب طب في جامعة القلمون"    << "معدلي هو"  << sum << std::endl;
+return 0;
+}
